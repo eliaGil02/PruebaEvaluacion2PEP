409aac027f7f346f925b65f7ab12373610fdd956
Elia <elia02.gil@gmail.com>
2026-02-23 12:47:31 +0100

    Añadida estructura base.html y aplicada herencia de plantillas en lista y detalle de tareas

--- tareas/views.py
+++ tareas/views.py
@@ -4,14 +4,14 @@ from django.views.generic import ListView, DetailView
 # Create your views here.
 
 
-class TareaListView(ListView):
+class ListaTareasView(ListView):
     # vista basada en clase que muestra todas las tareas
     model = Tarea  # modelo que se va a mostrar
     template_name = "tareas/lista_tareas.html"  # plantilla que se usa
     context_object_name = "tareas"  # nombre que usamos en el HTML
 
 
-class TareaDetailView(DetailView):
+class DetalleTareaView(DetailView):
     model = Tarea  # modelo del q se obtendrá el objeto
     template_name = "tareas/detalle_tarea.html"  # plantilla que se usa
     context_object_name = "tarea"  # nombre que usamos en el HTML
