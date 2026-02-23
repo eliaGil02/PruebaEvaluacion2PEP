208e2fb4ea26432b6993e75b550d5016313d27d5
Elia <elia02.gil@gmail.com>
2026-02-23 13:36:46 +0100

    Añadida funcionalidad de busqueda por titulo

--- tareas/views.py
+++ tareas/views.py
@@ -9,10 +9,22 @@ class ListaTareasView(ListView):
     model = Tarea  # modelo que se va a mostrar
     template_name = "tareas/lista_tareas.html"  # plantilla que se usa
     context_object_name = "tareas"  # nombre que usamos en el HTML
+    ordering = ["-fecha_creacion"]  # ordenar por fecha de creacion descendente
+
+    # sobreescribimos el metodo q obtiene los datos, q permite modificar la lista de tareas antes de enviarla
+    def get_queryset(self):
+        queryset = super().get_queryset()  # obtenemos todas las tareas
+        busqueda = self.request.GET.get(
+            "tareaBuscar"
+        )  # recogemos lo q el usuario escribe en el buscador
+        if busqueda:
+            # filtramos las tareas q tenga el titulo con el texto buscado
+            queryset = queryset.filter(titulo__icontains=busqueda)
+
+        return queryset  # devolvemos la lista final
 
 
 class DetalleTareaView(DetailView):
     model = Tarea  # modelo del q se obtendrá el objeto
     template_name = "tareas/detalle_tarea.html"  # plantilla que se usa
     context_object_name = "tarea"  # nombre que usamos en el HTML
-    ordering = ["-fecha_creacion"]  # ordenar por fecha de creacion descendente
