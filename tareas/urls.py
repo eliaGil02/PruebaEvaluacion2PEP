8549c4d5905ddf87c518d05ea08e07696999bbd9
Elia <elia02.gil@gmail.com>
2026-02-23 12:28:46 +0100

    Añadida vista DetalleTareaView y configuracion de URL para detalle de tareas

--- tareas/urls.py
+++ tareas/urls.py
@@ -5,9 +5,11 @@ Rutas específicas de esta app
 """
 
 from django.urls import path
-from .views import TareaListView
+from .views import ListaTareasView, DetalleTareaView
 
 urlpatterns = [
     # ruta principal que muestra la lista de tareas
-    path("", TareaListView.as_view(), name="lista_tareas"),
+    path("", ListaTareasView.as_view(), name="lista_tareas"),
+    # ruta que muestra el detalle de una tarea concreta
+    path("tarea/<int:pk>/", DetalleTareaView.as_view(), name="detalle_tarea"),
 ]
--- tareas/views.py
+++ tareas/views.py
@@ -1,5 +1,5 @@
 from .models import Tarea
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -9,3 +9,9 @@ class TareaListView(ListView):
     model = Tarea  # modelo que se va a mostrar
     template_name = "tareas/lista_tareas.html"  # plantilla que se usa
     context_object_name = "tareas"  # nombre que usamos en el HTML
+
+
+class TareaDetailView(DetailView):
+    model = Tarea  # modelo del q se obtendrá el objeto
+    template_name = "tareas/detalle_tarea.html"  # plantilla que se usa
+    context_object_name = "tarea"  # nombre que usamos en el HTML
