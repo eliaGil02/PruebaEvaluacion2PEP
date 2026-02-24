931eb4fc33c60550edb9e10d35331247129c11bf
Riz-baig <baigrizmirza@gmial.com>
2026-02-24 14:17:07 +0100

    Añadido template para crear tarea

--- tareas/urls.py
+++ tareas/urls.py
@@ -6,10 +6,13 @@ Rutas específicas de esta app
 
 from django.urls import path
 from .views import ListaTareasView, DetalleTareaView
+from .views import CrearTareaView #+importamos la clase desde tarea/view.py
 
 urlpatterns = [
     # ruta principal que muestra la lista de tareas
     path("", ListaTareasView.as_view(), name="lista_tareas"),
     # ruta que muestra el detalle de una tarea concreta
     path("tarea/<int:pk>/", DetalleTareaView.as_view(), name="detalle_tarea"),
+    # Ruta para crear una nueva tarea
+    path("crear/", CrearTareaView.as_view(), name="crear_tarea"),
 ]
